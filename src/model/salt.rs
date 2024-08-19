8ccfcdce4e652aa11b5a48423579ad0bf2243b8b
esakkimuthu-p <esakkimuthu556@gmail.com>
2024-08-19 18:05:58 +0530

    upd

--- src/model/salt.rs
+++ src/model/salt.rs
@@ -5,23 +5,21 @@ pub struct Salt;
 impl Salt {
     pub async fn create(mongodb: &Database, postgres: &PostgresClient) {
         let mut cur = mongodb
-            .collection::<Document>("pharma_salts")
-            .find(
-                doc! {},
-                find_opts(
-                    doc! {"createdBy": 0, "createdAt": 0, "updatedAt": 0, "updatedBy": 0},
-                    doc! {"_id": 1},
-                ),
-            )
+            .collection::<Document>("salts")
+            .find(doc! {}, None)
             .await
             .unwrap();
         let mut updates = Vec::new();
         while let Some(Ok(d)) = cur.next().await {
             let object_id = d.get_object_id("_id").unwrap();
+            let drug = d
+                .get_string("h")
+                .or(d.get_string("h1"))
+                .or(d.get_string("nc"));
             let id: i32 = postgres
                 .query_one(
-                    "INSERT INTO pharma_salt (name) VALUES ($1) returning id",
-                    &[&d.get_str("name").unwrap()],
+                    "INSERT INTO pharma_salt (name, drug_category) VALUES ($1,$2) returning id",
+                    &[&d.get_str("name").unwrap(), &drug],
                 )
                 .await
                 .unwrap()
